5151755c96a2255cbccce662381909246b82c1e2
Vigneshwar <vigvenkat@m800.com>
2020-04-11 22:15:07 +0530

    site updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "Inspiration from within !",
         "excerpt":"In my opinion, clearly no person other than Steve Jobs will be remembered by the world for what he did to technology and design. No other batsman other than Sachin Tendulkar is rightly portrayed to be the ‘God of Cricket’ for what he did to the game and for Indian...","categories": ["Ideas","Philosophy"],
         "tags": ["Philosophy","Ideas","SelfImprovement"],
-        "url": "https://themoralpanda.github.io/ideas/philosophy/inspiration-from-within/",
+        "url": "https://themoralpanda.github.io/https://themoralpanda.github.io/ideas/philosophy/inspiration-from-within/",
         "teaser":null},{
         "title": "Stop The Goddamn Car !",
         "excerpt":"Ever felt really bad after a stupid argument? over a stupid topic? with your close ones? Ever felt so exhausted after such bad moments and started to regret things? I also have encountered such moments. I recently got an important advice from my dad which was more like a solution...","categories": ["Ideas","Philosophy"],
         "tags": ["Philosophy","Ideas","SelfImprovement"],
-        "url": "https://themoralpanda.github.io/ideas/philosophy/stop-the-goddamn-car/",
+        "url": "https://themoralpanda.github.io/https://themoralpanda.github.io/ideas/philosophy/stop-the-goddamn-car/",
         "teaser":null},{
         "title": "A 24 year old's struggle to learn Music",
         "excerpt":"Firstly listen to the below wonderful piece composed by Ludovico Einaudi Did you notice the talent? The involvement? The fingers moving effortlessly with absolute passion for the tune? The background orchestra supporting like pillars of a beautiful monument? The violins and cellos. This is a thing of a beauty. Now...","categories": ["Personal","Experience"],
         "tags": ["Experience","Motivation","Introspection"],
-        "url": "https://themoralpanda.github.io/personal/experience/A-24yr-old's-struggle-to-learn-Music/",
+        "url": "https://themoralpanda.github.io/https://themoralpanda.github.io/personal/experience/A-24yr-old's-struggle-to-learn-Music/",
         "teaser":null},{
         "title": "Here is why I am losing interests on my country and I dont need help",
         "excerpt":"I am an Indian. A 24 year old. Spent all my 24 years so far in India except few vacations. Right through my school days, I was taught Patriotism via books, Prayers, Sports routines and early celebrations of historically important days. During those days, I felt it. The pride, the...","categories": ["Opinion","Society"],
         "tags": ["Opinions","Society","Musing"],
-        "url": "https://themoralpanda.github.io/opinion/society/Here-is-why-I-am-losing-interests-in-my-country-and-I-don't-need-help!/",
+        "url": "https://themoralpanda.github.io/https://themoralpanda.github.io/opinion/society/Here-is-why-I-am-losing-interests-in-my-country-and-I-don't-need-help!/",
         "teaser":null}]
